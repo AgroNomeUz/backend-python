8ad96f883b1bb631c550977c3dbe5740c83868c4
Ulugbek <ulugbek@framework.tail3c3cea.ts.net>
2026-08-10 22:13:21 +0500

    csrf

--- agro/settings.py
+++ agro/settings.py
@@ -163,3 +163,4 @@ if ENVIRONMENT == 'production':
     SESSION_COOKIE_SECURE = True
     CSRF_COOKIE_SECURE = True
     SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+    DEBUG = False
